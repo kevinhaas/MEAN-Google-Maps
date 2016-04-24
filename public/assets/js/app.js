cf9d7f20510732d98dcc8180fab699cd33b4397a
Kevo <kevo548@gmail.com>
2016-04-24 15:04:08 -0400

    query logic complete

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,2 +1,18 @@
 // Declares the initial angular module "meanMapApp". Module grabs other controllers and services.
-var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice']);
+var app = angular.module('meanMapApp', ['addCtrl', 'geolocation', 'gservice', 'ngRoute'])
+
+// Configs Angular routing - shows the relevant view and controller when needed
+.config(function($routeProvider) {
+
+    // Join Team Control Panel
+    $routeProvider.when('/join', {
+        controller:'addCtrl',
+        templateUrl: '../partials/addForm.html'
+
+        // Find teammates Control Panel
+    }).when('/find', {
+        templateUrl: '../partials/queryForm.html'
+
+        // All else forward to the Join Team Control Panel
+    }).otherwise({redirectTo:'/join'})
+});
--- public/assets/js/routes.js
+++ public/assets/js/routes.js
@@ -38,4 +38,38 @@ module.exports = function(app) {
             res.json(req.body);
         });
     });
-};  
+
+    // Queries the user DB
+    app.post('/query/', function(req, res){
+
+        // Grab all of the query parameters from the body.
+        var lat             = req.body.latitude;
+        var long            = req.body.longitude;
+        var distance        = req.body.distance;
+
+        // Opens a generic Mongoose Query. Depending on the post body we will...
+        var query = User.find({});
+
+        // ...include filter by Max Distance (converting miles to meters)
+        if(distance){
+
+            // Using MongoDB's geospatial querying features. (Note how coordinates are set [long, lat]
+            query = query.where('location').near({ center: {type: 'Point', coordinates: [long, lat]},
+
+                // Converting meters to miles. Specifying spherical geometry (for globe)
+                maxDistance: distance * 1609.34, spherical: true});
+        }
+
+        // ... Other queries will go here ...
+
+        // Execute Query and Return the Query Results
+        query.exec(function(err, users){
+            if(err)
+                res.send(err);
+
+            // If no errors, respond with a JSON of all users that meet the criteria
+            res.json(users);
+        });
+    });
+
+};
